3e3e564701dab43470ea8c67c93112cc5d5a960e
stevef <stevefrazee123@gmail.com>
2026-05-16 14:34:31 -0400

    redo windows mapping to fix testing bug

--- src/make_selection/windows.py
+++ src/make_selection/windows.py
@@ -19,25 +19,25 @@ def isSearchable(char: int) -> bool:
     return (32 <= char and char <= 126)
     
 def getChar() -> tuple[KeyCode|None, str|None]:
+    key_code = None
+    char = None
+
     key_press = ord(msvcrt.getch())
     if key_press == SPECIAL_KEY:
         key_press = ord(msvcrt.getch())
-
-    key_code = None
-    char = None
-    if key_press == ARROW_UP:
-        key_code = KeyCode.UP
-    elif key_press == ARROW_DOWN:
-        key_code = KeyCode.DOWN
+        if key_press == ARROW_UP:
+            key_code = KeyCode.UP
+        elif key_press == ARROW_DOWN:
+            key_code = KeyCode.DOWN
     elif key_press == ENTER:
         key_code = KeyCode.SELECT
     elif key_press == CTL_C:
         key_code = KeyCode.CANCEL
-    elif isSearchable(key_press):
-        key_code = KeyCode.SEARCHABLE
-        char = chr(key_press)
     elif key_press == BACKSPACE:
         key_code = KeyCode.DELETE_CHAR
     elif key_press == TAB:
         key_code = KeyCode.SELECT_MULTI
+    elif isSearchable(key_press):
+        key_code = KeyCode.SEARCHABLE
+        char = chr(key_press)
     return key_code, char
